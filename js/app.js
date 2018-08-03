d9d4f8f7e875d787b5ab4bd7ed1251a42612d83a
Somto Onyekwere <somtochionyekwere@gmail.com>
2018-08-03 12:41:12 +0100

    Adds basic matching functionality

--- js/app.js
+++ js/app.js
@@ -12,26 +12,33 @@
  */
 const deck = document.getElementById("deck");
 const card = document.querySelectorAll(".card");
+let cardsOpen;
 
 function displayCards() {
     while(deck.hasChildNodes()) {
         deck.removeChild(deck.childNodes[0]);
     }
-    console.log(deck.innerHTML)
+    //console.log(deck.innerHTML)
     shuffle(allCards);
     const docFrag = document.createDocumentFragment();
-    const newList = document.createElement("li"); 
-    newList.classList.add("card");
+    // newList.classList.add("card");
+    // const newList = document.createElement("li"); 
     for(let card of allCards) {
         const newList = document.createElement("li");
-        newList.classList.add("card");
+        newList.classList.add("open","card", "show")
         newList.innerHTML = '<i class="fa fa-' + card + '"></i>'
         docFrag.appendChild(newList);
     }
     deck.appendChild(docFrag);
-    console.log(deck.innerHTML);
- }
+    // console.log(deck.innerHTML);
+}
 
+ function removeCards(open) {
+    open = document.querySelectorAll(".open");
+     open.forEach(card => {
+         card.classList.remove('open', 'show')
+     })
+ }
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
@@ -48,7 +55,9 @@ function shuffle(array) {
 }
 
 displayCards();
-
+setTimeout(() => {
+    removeCards(cardsOpen)
+}, 5000);
 
 /*
  * set up the event listener for a card. If a card is clicked:
@@ -62,31 +71,96 @@ displayCards();
  */
 
 deck.addEventListener("click", function handleCardClick(evt){
+
     displaySymbol(evt);
     addOpenCards(evt);
+    matchOpenCards();
+    checkAllMatch();
 });
 
-//fumction to display symbol
+//function to display symbol
 function displaySymbol(evt) {
     const clickedCard = evt.target;
     clickedCard.classList.add("open", "show");
     //console.log(clickedCard)
 }
 
-const openCards = [];
 
 function addOpenCards(evt) {
+    const openCards = [];
     const clickedCard= evt.target       
     const allCardArray = clickedCard.firstElementChild.className.split(" ");
     const cardArray = allCardArray[1].split("-");
     const specificCard = cardArray[1];
     openCards.push(specificCard);
-    console.log(openCards)
+    // console.log(openCards);
+}
+
+function matchOpenCards(){
+    let openCards = document.querySelectorAll(".open")
+    if(openCards.length > 1){
+        if(openCards[0].innerHTML !== openCards[1].innerHTML){
+            nonMatchingCards(openCards)
+            incrementCounter(openCards)
+            openCards = [];
+            // console.log(openCards)
+            
+            
+        } else {
+            matchingCards(openCards)
+            incrementCounter(openCards)
+            openCards = []
+        }
+    }
+    
+    
 }
 
 
+function matchingCards(open) {
+    open.forEach( card => {
+        card.classList.add('match');
+        card.classList.remove("open");
+    })
+    
+    
+}
 
- 
+function nonMatchingCards( open ){
+    
+    open.forEach( card => {
+        card.classList.add('non-match');
+        
+        setTimeout(()=> {
+            card.classList.remove("open", "show", "non-match")
+            
+        }, 1000)
+    })
+    
+	
+}
+
+function incrementCounter(openCards) {
+    // let openCards = document.querySelectorAll(".open");
+    if (openCards.length === 2) {
+        let counter = document.querySelector(".moves").textContent;
+        console.log(counter);
+        counter = parseInt(counter) + 1;
+        // console.log(`this is the new ${counter}`)
+        document.querySelector(".moves").textContent = counter;
+    
+        // incrementCounter(openCards)
+    
+    }
+}
+
+function checkAllMatch() {
+    const matchedCard = document.querySelectorAll(".match");
+    if (matchedCard.length === 16) {
+        console.log("All Matched!")
+    }
+}
+
 
 
 
